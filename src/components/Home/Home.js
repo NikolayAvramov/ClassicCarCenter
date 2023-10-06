0b342caf7ec6345b178f80cb973a220910a8621e
Nikolay <106226408+NikolayAvramov@users.noreply.github.com>
2023-10-06 10:25:53 +0300

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import headerImg from "../../fonts/Classic-Car-Collecting-03.png";
 export function Home() {
 	return (
 		<div className={HomeCss.dynamic}>
-			<h2 className={HomeCss.title}>Classic Car Center</h2>
+			<h2 className={HomeCss.title}>Classic Car </h2>
 
 			<ImgSlider className={HomeCss.slides} />
 			<p className={HomeCss.text}>Classic Car Center is the best place to found your new classic car or to sell your.Here you can found many announce on diferent makes and models and connect directly with the seller. Enjoy to your tour in our Center.</p>
